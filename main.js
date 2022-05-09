8aec07414fcb7f130630d3e8b4811a410f9743b9
Jaric De Becker <jaric.debecker@gmail.com>
2022-05-09 09:57:06 +0200

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+import "./style.css";
+import store from "./data";
+import { decrement, increment } from "./data/counter";
+import { addTodo } from "./data/todo";
+function render() {
+  document.querySelector("#app").innerHTML = `
+  <h1>${store.getState().counterState.counter}</h1>
+`;
+
+  document.querySelector("#ul").innerHTML = store
+    .getState()
+    .todoState.map((todo) => `<li>${todo.todo}</li>`)
+    .join("");
+}
+
+render();
+
+store.subscribe(render);
+
+document.getElementById("dec").onclick = function () {
+  store.dispatch(decrement());
+};
+
+document.getElementById("inc").onclick = function () {
+  store.dispatch(increment());
+};
+
+const form = document.querySelector("form");
+form.onsubmit = function (e) {
+  e.preventDefault();
+  store.dispatch(addTodo(form.elements["veld"].value));
+  form.reset();
+};
